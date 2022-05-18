3b5191776c3d313a58596c7af9659891f3150d8d
mile0105 <mt010595@gmail.com>
2022-05-18 18:00:43 +0200

    Added the documents table script

---
+++ src/main/resources/db/V1.0.7__add_documents.sql
@@ -0,0 +1,14 @@
+create table documents (
+  id serial not null primary key,
+  owner_id serial not null,
+  receiver_first_name text not null,
+  receiver_phone_number text not null,
+  format varchar(10) not null,
+  constraint owner_id_constraint foreign key(owner_id) references users(id)
+);
+
+
+alter table trips add column trip_price decimal not null default 0;
+alter table trips add column document_price decimal not null default 0;
+
+alter table trips add column maximum_number_of_documents integer not null default 10;
